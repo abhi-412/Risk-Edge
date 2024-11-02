4b4bc55d7233c56d713961aea935a7cb9d24c659
Abhay <yabhay@iiitmanipur.ac.in>
2024-11-02 17:28:29 +0530

    new commit-2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -154,7 +154,7 @@ const Home = ({pageOneRef, pageTwoRef, pageThreeRef}) => {
                 
             </div>
             </div>
-            <div className="flex">
+            <div className="flex md:hidden">
                 <a href="#" className="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
