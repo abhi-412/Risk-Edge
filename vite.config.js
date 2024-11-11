a747b246242e72a814236573b4f35b6ffabb5277
Abhay Pratap Yadav <128727269+abhi-412@users.noreply.github.com>
2024-11-11 21:36:40 +0530

    Update vite.config.js
    jj

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['*/.glb'],
-  server: {
-    https: {
-      key: fs.readFileSync('/home/nitpicks/.acme.sh/riskedgesolutions.com/riskedgesolutions.com.key'),
-      cert: fs.readFileSync('/home/nitpicks/.acme.sh/riskedgesolutions.com/fullchain.cer')
-    },
-    host: '0.0.0.0',
-    port: 3000,
-  },
+  assetsInclude: ['**/*.glb'],
   build: {
-    chunkSizeWarningLimit: 2000, // Sets limit to 2000 kB
+    chunkSizeWarningLimit: 2000, // Sets limit to 1000 kB
   },
-});
+})
